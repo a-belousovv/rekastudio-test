23f13c5a884fc52ac2166f3c82231e47cdaffe1f
a-belousovv <aleksander.belousovv@gmail.com>
2024-10-15 23:14:03 +0300

    first

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import App from './App.vue';
 import './style.scss';
+// @ts-ignore
+import App from './App.vue';
 const app = createApp(App);
 const pinia = createPinia();
 
